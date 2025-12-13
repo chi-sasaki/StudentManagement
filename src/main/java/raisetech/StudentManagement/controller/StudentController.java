4b4d1fd5ef8cdc00e572bd7c5623e381e1e2c725
c-sasaki <plog.chi.sasa369@gmail.com>
2025-12-13 19:54:40 +0900

    converterの追加 データーベースとStudentクラスにremarkとisDeletedを追加

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -3,8 +3,10 @@ package raisetech.StudentManagement.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import raisetech.StudentManagement.controller.converter.StudentConverter;
 import raisetech.StudentManagement.date.Course;
 import raisetech.StudentManagement.date.Student;
+import raisetech.StudentManagement.domain.StudentDetail;
 import raisetech.StudentManagement.service.StudentService;
 
 import java.util.List;
@@ -13,15 +15,20 @@ import java.util.List;
 public class StudentController {
 
     private StudentService service;
+    private StudentConverter converter;
 
     @Autowired
-    public StudentController(StudentService service) {
+    public StudentController(StudentService service, StudentConverter converter) {
         this.service = service;
+        this.converter = converter;
     }
 
     @GetMapping("/studentList")
-    public List<Student> getStudentList() {
-        return service.searchStudentList();
+    public List<StudentDetail> getStudentList() {
+        List<Student> students = service.searchStudentList();
+        List<Course> courses = service.searchCourseList();
+
+        return converter.convertStudentDetails(students, courses);
     }
 
     @GetMapping("/courseList")
